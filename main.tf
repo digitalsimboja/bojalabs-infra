9d962e9298d8695128b7ed22cc1a271400cb98cf
digitalsimboja-dm <sunday.mgbogu@datamellon.com>
2025-07-01 08:44:41 +0100

    update infra

--- main.tf
+++ main.tf
@@ -8,50 +8,82 @@ terraform {
 }
 
 provider "aws" {
-    region = var.region
-    profile = "dev"
+  region  = var.region
+  profile = "dev"
 }
 
-resource "aws_s3_bucket" "data_segmentation_scripts" {
-    bucket = var.s3_glue_scripts_bucket_name
-    force_destroy = true
+resource "aws_s3_bucket" "data_categorization_script" {
+  bucket        = var.s3_categorize_bucket_name
+  force_destroy = true
 }
 
-resource "aws_s3_object" "glue_script" {
-    bucket = aws_s3_bucket.data_segmentation_scripts.id
-    key = "glue_scripts/data-segmentation.py"
-    source = "${path.module}/glue_scripts/data-segmentation.py"
-    etag = filemd5("${path.module}/glue_scripts/data-segmentation.py")
+resource "aws_s3_object" "categorization_script" {
+  bucket = aws_s3_bucket.data_categorization_script.id
+  key    = "glue_scripts/categorize.py"
+  source = "${path.module}/glue_scripts/categorize.py"
+  etag   = filemd5("${path.module}/glue_scripts/categorize.py")
+}
+
+resource "aws_s3_bucket" "data_segmentation_script" {
+  bucket        = var.s3_segemtation_bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_object" "segmentation_script" {
+  bucket = aws_s3_bucket.data_segmentation_script.id
+  key    = "glue_scripts/segment.py"
+  source = "${path.module}/glue_scripts/segment.py"
+  etag   = filemd5("${path.module}/glue_scripts/segment.py")
 }
 
 resource "aws_iam_role" "glue_role" {
-    name = "data-segmentation-glue-role"
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = "sts:AssumeRole"
-                Effect = "Allow"
-                Sid    = "AssumeGlueRole"
-                Principal = {
-                    Service = "glue.amazonaws.com"
-                }
-            },
-        ]
-    })
+  name = "data-categorization-segmentation-glue-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = "AssumeGlueRole"
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "glue_service" {
-    role       = aws_iam_role.glue_role.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
 
-resource "aws_glue_job" "data_segmentation_job" {
+resource "aws_glue_job" "categorize_data_job" {
+  name     = "data-categorization-job"
+  role_arn = aws_iam_role.glue_role.arn
+  command {
+    name            = "glueetl"
+    script_location = "s3://${aws_s3_bucket.data_categorization_script.bucket}/glue_scripts/categorize.py"
+    python_version  = "3"
+  }
+  glue_version      = "4.0"
+  number_of_workers = 2
+  worker_type       = "G.1X"
+  execution_property {
+    max_concurrent_runs = 1
+  }
+  tags = {
+    Environment = "dev"
+  }
+
+}
+
+resource "aws_glue_job" "segment_data_job" {
   name     = "data-segmentation-job"
   role_arn = aws_iam_role.glue_role.arn
   command {
     name            = "glueetl"
-    script_location = "s3://${aws_s3_bucket.data_segmentation_scripts.bucket}/glue_scripts/data-segmentation.py"
+    script_location = "s3://${aws_s3_bucket.data_segmentation_script.bucket}/glue_scripts/segment.py"
     python_version  = "3"
   }
   glue_version      = "4.0"
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,21 @@
-output "glue_job_name" {
-  value = aws_glue_job.data_segmentation_job.name
+output "categorize_glue_job_name" {
+  value = aws_glue_job.categorize_data_job.name
 }
 
-output "glue_script_location" {
-  value = "s3://${aws_s3_bucket.data_segmentation_scripts.bucket}/${aws_s3_object.glue_script.key}"
+output "categorize_glue_script_location" {
+  value = "s3://${aws_s3_bucket.data_categorization_script.bucket}/${aws_s3_object.categorization_script.key}"
 }
+
+output "segmentation_glue_job_name" {
+  value = aws_glue_job.segment_data_job.name
+}
+
+output "segmentation_glue_script_location" {
+  value = "s3://${aws_s3_bucket.data_segmentation_script.bucket}/${aws_s3_object.segmentation_script.key}"
+}
+
+
+# categorize_glue_job_name = "data-categorization-job"
+# categorize_glue_script_location = "s3://data-categorization-scripts/glue_scripts/categorize.py"
+# segmentation_glue_job_name = "data-segmentation-job"
+# segmentation_glue_script_location = "s3://data-segmentation-scripts/glue_scripts/segment.py"
